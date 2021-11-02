5aa6b2cc0edf6654dd55d513654c465fd130eb32
bneveu <Bertrand.Neveu@enpc.fr>
2021-11-02 17:24:08 +0100

    adding MinlpSmearSum and MinlpSmearSumRelative bisectors

---
+++ src/bisector/ibex_MinlpSmearSumRelative.h
@@ -0,0 +1,59 @@
+//============================================================================
+//                                  I B E X                                   
+// File        : Smearsumrelative bisector variant for MINLP optimization 
+// Author      : Bertrand Neveu
+// Copyright   : IMT Atlantique (France)
+// License     : See the LICENSE file
+// Created     : Aug 19, 2021
+// Last Update : Aug 19, 2021
+//============================================================================
+
+#ifndef __IBEX_MINLP_SMEARSUMREL_H__
+#define __IBEX_MINLP_SMEARSUMREL_H__
+
+#include "ibex_Bsc.h"
+#include "ibex_SmearFunction.h"
+#include "ibex_System.h"
+#include "ibex_LargestFirst.h"
+
+namespace ibex {
+
+/**
+ * \ingroup bisector
+ *
+ * \brief smear sum relative bisector for minlp
+ *
+ */
+class MinlpSmearSumRelative : public SmearFunction {
+public:
+
+	/**
+	 * \brief Create a bisector with largest-first heuristic.
+	 *
+	 */
+  MinlpSmearSumRelative(System& sys,  double prec,   LargestFirst& lf, bool gb=true);
+
+	/**
+	 * \brief Create a bisector with largest first heuristic.
+	 *
+	 * \param prec             - see #Bsc::Bsc(double).
+	 * \param ratio (optional) - the ratio between the diameters of the left and the right parts of the
+	 *                           bisected interval. Default value is 0.45.
+	 */
+  MinlpSmearSumRelative(System& sys,const Vector& prec,LargestFirst& lf);
+
+	/**
+	 * \brief Return next variable to be bisected.
+	 *
+	 * called by Bsc::bisect(...)
+	 */
+  int var_to_bisect(IntervalMatrix& J, const IntervalVector& box) const;
+	
+
+
+	  
+};
+
+}// end namespace ibex
+
+#endif // __IBEX_MINLP_SMEARSUMREL_H__
